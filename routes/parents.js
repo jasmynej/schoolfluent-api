cc4dc44dd6d858ff8d74f4fb5a510a23403d7854
Jasmyne Jean-Remy <jasmynej14@gmail.com>
2022-03-16 00:57:38 -0400

    add/login for all people

--- routes/parents.js
+++ routes/parents.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../db/connection")
+const bcrypt = require('bcryptjs');
 router.get('/',async function(req,res){
     var parents = await db.query("SELECT * FROM parents")
     res.send(parents)
@@ -11,4 +12,25 @@ router.get('/:id',async function(req,res){
     res.json(parent)
 })
 
+router.post("/add",async function(req,res){
+    const pwd = bcrypt.hashSync("password",8)
+    const parent = await db.query("INSERT INTO parents (first_name,last_name,email,pwd_hash) VALUES($1,$2,$3,$4) RETURNING id",
+    [req.body.first_name,req.body.last_name,req.body.email,pwd]
+    )
+    res.json(parent)
+})
+
+router.post("/login",async function(req,res){
+    const parent = await db.query("SELECT * FROM parents WHERE email = $1",req.body.email)
+    if (parent.length === 0){
+        res.json({msg:"user does not exist"})
+    }
+    else if (!bcrypt.compareSync(req.body.pwd,parent[0].pwd_hash)){
+        res.json({msg:"password incorrect"})
+    }
+    else {
+        res.json(parent)
+    }
+  
+})
 module.exports = router
--- routes/teacher_admin.js
+++ routes/teacher_admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../db/connection")
+const bcrypt = require('bcryptjs');
 
 router.get('/',async function(req,res){
     var teachers = await db.query("SELECT * FROM teacher_admin")
@@ -11,4 +12,27 @@ router.get('/:id',async function(req,res){
     var teacher_admin = await db.query("SELECT * FROM teacher_admin WHERE id = $1",req.params.id)
     res.json(teacher_admin)
 })
+
+router.post("/add",async function(req,res){
+    const pwd = bcrypt.hashSync("password",8)
+    const teacher_admin = await db.query("INSERT INTO teacher_admin (first_name,last_name,email,pwd_hash,role,admin) VALUES($1,$2,$3,$4,$5,$6) RETURNING id",
+    [req.body.first_name,req.body.last_name,req.body.email,pwd,req.body.role,req.body.isAdmin]
+    )
+    res.json(teacher_admin)
+})
+
+router.post("/login",async function(req,res){
+    const teacher_admin = await db.query("SELECT * FROM teacher_admin WHERE email = $1",req.body.email)
+    if (teacher_admin.length === 0){
+        res.json({msg:"user does not exist"})
+    }
+    else if (!bcrypt.compareSync(req.body.pwd,teacher_admin[0].pwd_hash)){
+        res.json({msg:"password incorrect"})
+    }
+    else {
+        res.json(teacher_admin)
+    }
+  
+})
+
 module.exports = router
